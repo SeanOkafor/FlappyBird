dfa1299ae105be9c8617c6499e36c5b9f2cb450d
SeanOkafor <126804794+SeanOkafor@users.noreply.github.com>
2023-09-07 23:38:33 +0100

    new changes

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("I hate this place");
-        System.out.println("I also hate black people");
+        System.out.println("I also hate black people!");
     }
 }
